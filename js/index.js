691f70f4c86db5bf8017fb473d89d22ca7a8cd97
Yomaira Escano <yjescano@gmail.com>
2018-09-11 04:20:57 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -25,6 +25,14 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div')
 
 function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
+function bubble(e) {
   e.stopPropagation()
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
