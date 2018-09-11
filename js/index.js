0a35b67695a1f8c8e8057d40a6edeb382e21e4d3
Yomaira Escano <yjescano@gmail.com>
2018-09-11 04:13:43 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 
-// const main = document.getElementById('main')
-// 
-// main.addEventListener('click', function(event) {
-//   alert('I was clicked!')
-// })
-// 
-// const input = document.querySelector('input')
-// 
-// input.addEventListener('keydown', function(e) {
-//   if (e.which === 71) {
-//     console.log('default prevented');
-//     return e.preventDefault()
-//   } else {
-//     console.log('Not a "g"')
-//   }
-// })
-// 
-// let divs = document.querySelectorAll('div')
-// 
-// function bubble(e) {
-//   e.stopPropagation()
-//   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-// }
-// 
-// for (let i = 0; i < divs.length; i++) {
-//   divs[i].addEventListener('click', bubble)
-// }
-// 
-// function capture(e) {
-//   console.log(this.firstChild.nodeValue.trip() + ' captured')
-// }
-// 
-// for (let i = 0; i < divs.length; i++) {
-//   divs[i].addEventListener('click', capture, true);
-// }
+const main = document.getElementById('main')
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault()
+  } else {
+    console.log('Not a "g"')
+  }
+})
+
+let divs = document.querySelectorAll('div')
+
+function bubble(e) {
+  e.stopPropagation()
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble)
+}
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trip() + ' captured')
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
+}
